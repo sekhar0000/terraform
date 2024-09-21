ddfe311c745514b350539f8add898ea3602da9fe
sekharreddy.ju <sekharreddy.ju@gmail.com>
2024-09-21 08:45:03 +0530

    ec2.tf updated file commiting

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -21,9 +21,15 @@ resource "aws_security_group" "allow_ssh_terraform" {
   tags = {
     Name = "RAJA"
   }
+}
+  resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    
+    tags = {
+        Name ="Raja_Ec2"
+    }
 
-
-
-
-
-}  
+}
+  
